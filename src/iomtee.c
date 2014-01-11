d585a28aff53d6ad2e8407fb4e513c8a05a9dee8
Andrea Guzzo <xant@xant.net>
2014-01-11 14:51:49 +0000

    fixed a bug showing when the write-pointer and the minimum-read-pointer are equal

--- src/iomtee.c
+++ src/iomtee.c
@@ -37,9 +37,9 @@ static int iomtee_write_buffer(iomtee_t *tee, void *data, int len)
             min_rofx = tee->fds[i].rofx;
     }
 
-    int free_space = abs(tee->wofx - min_rofx);
+    int free_space = abs(tee->wofx - min_rofx)+1;
     int write_len = free_space > len ? len : free_space;
-    if (tee->wofx < min_rofx) {
+    if (tee->wofx <= min_rofx) {
         memcpy(&tee->buf[tee->wofx], data, write_len);
         tee->wofx += write_len;
     } else {
